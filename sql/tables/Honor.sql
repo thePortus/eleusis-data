9c245e8fd1b61809079891064585a8459e6cd285
David J. Thomas <dave.a.base@gmail.com>
2017-06-06 13:25:00 -0400

    honor.sql updated

--- sql/tables/Honor.sql
+++ sql/tables/Honor.sql
@@ -6,6 +6,7 @@ CREATE TABLE public."Honor"
 (
   "ID" integer NOT NULL,
   "Honor" character varying(100),
+  "Origin" character varying(100),
   "Category" character varying(100),
   "Type" character varying(100),
   "Notes" character varying(1000),
