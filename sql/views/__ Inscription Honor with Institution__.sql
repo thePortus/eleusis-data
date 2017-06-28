2d9e2e667d1e82854e7f54ebe19c1f6d65c9d08b
David J. Thomas <dave.a.base@gmail.com>
2017-06-28 15:14:42 -0400

    inscription based views added

---
+++ sql/views/__ Inscription Honor with Institution__.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE VIEW public."__ Inscription Honor with Institution __" AS
+SELECT DISTINCT inscription."IE" AS "IE",
+                honor."Honor" AS "Honor",
+                institution."Institution" AS "Institution",
+                inscription."Object Type" AS "Object Type",
+                inscription."Inscription Type" AS "Inscription Type",
+                inscription."Location" AS "Location",
+                inscription."Date" AS "Date",
+                inscription."Date Span" AS "Date Span",
+                inscription."Word Count" AS "Word Count",
+                inscription."Character Count" AS "Character Count",
+                inscription."Text" AS "Text",
+                honor."Type" AS "Honor Type",
+                honor."Category" AS "Honor Origin",
+                institution."Category" AS "Institution Origin",
+                institution."Type" AS "Institution Type",
+                inscription."ID" AS "Inscription ID",
+                institution."ID" AS "Institution ID",
+                honor."ID" AS "Honor ID"
+FROM public."Honor in Inscription" AS honor_inscription
+LEFT JOIN public."Institution Honor" AS institution_honor ON honor_inscription."Honor ID" = institution_honor."Honor ID"
+INNER JOIN public."Inscription with Text"() AS inscription ON honor_inscription."Inscription ID" = inscription."ID"
+INNER JOIN public."Honor" AS honor ON institution_honor."Honor ID" = honor."ID"
+LEFT JOIN public."Institution" AS institution ON institution_honor."Institution ID" = institution."ID"
+ORDER BY inscription."ID",
+         institution."ID",
+         honor."ID"
---
+++ sql/views/__ Inscription Sponsor __.sql
@@ -0,0 +1,59 @@
+CREATE OR REPLACE VIEW public."__ Inscription Sponsor __" AS
+SELECT DISTINCT inscription."IE" AS "IE",
+                sponsor."Person" AS "Sponsor",
+                inscription."Object Type" AS "Object Type",
+                inscription."Inscription Type" AS "Inscription Type",
+                inscription."Location" AS "Location",
+                inscription."Date" AS "Date",
+                inscription."Date Span" AS "Date Span",
+                inscription."Word Count" AS "Word Count",
+                inscription."Character Count" AS "Character Count",
+                inscription."Text" AS "Text",
+                'Person' AS "Sponsor Class",
+                sponsor."Category" AS "Sponsor Origin",
+                CASE
+                    WHEN sponsor."Category" = 'Athenian'
+                         AND sponsor."Roman Citizen" = FALSE THEN 'Athenian without Roman Citizenship'
+                    WHEN sponsor."Category" = 'Athenian'
+                         AND sponsor."Roman Citizen" = TRUE THEN 'Athenian with Roman Citizenship'
+                    ELSE 'Non-Athenian'
+                END AS "Sponsor Status",
+                NULL AS "Sponsor Type",
+                sponsor."Gender" AS "Sponsor Gender",
+                sponsor."Family" AS "Sponsor Family",
+                sponsor."Extended" AS "Sponsor Extended Family",
+                sponsor."Deme" AS "Sponsor Deme",
+                inscription."ID" AS "Inscription ID",
+                sponsor."ID" AS "Sponsor ID",
+                sponsor."Uncertain Person" AS "Sponsor Uncertainty"
+FROM public."Person in Inscription" AS sponsor_inscription
+INNER JOIN public."Inscription with Text"() AS inscription ON sponsor_inscription."Inscription ID" = inscription."ID"
+INNER JOIN public."Person" AS sponsor ON sponsor_inscription."Person ID" = sponsor."ID"
+WHERE sponsor_inscription."Role" = 'Sponsor'
+UNION ALL
+SELECT DISTINCT inscription."IE" AS "IE",
+                sponsor."Institution" AS "Sponsor",
+                inscription."Object Type" AS "Object Type",
+                inscription."Inscription Type" AS "Inscription Type",
+                inscription."Location" AS "Location",
+                inscription."Date" AS "Date",
+                inscription."Date Span" AS "Date Span",
+                inscription."Word Count" AS "Word Count",
+                inscription."Character Count" AS "Character Count",
+                inscription."Text" AS "Text",
+                'Institution' AS "Sponsor Class",
+                sponsor."Category" AS "Sponsor Origin",
+                NULL AS "Sponsor Status",
+                sponsor."Type" AS "Sponsor Type",
+                NULL AS "Sponsor Gender",
+                NULL AS "Sponsor Family",
+                NULL AS "Sponsor Extended Family",
+                NULL AS "Sponsor Deme",
+                inscription."ID" AS "Inscription ID",
+                sponsor."ID" AS "Sponsor ID",
+                sponsor_inscription."Uncertain" AS "Sponsor Uncertainty"
+FROM public."Institution Sponsorship" AS sponsor_inscription
+INNER JOIN public."Inscription with Text"() AS inscription ON sponsor_inscription."Inscription ID" = inscription."ID"
+INNER JOIN public."Institution" AS sponsor ON sponsor_inscription."Institution ID" = sponsor."ID"
+ORDER BY "Inscription ID",
+         "Sponsor ID"
