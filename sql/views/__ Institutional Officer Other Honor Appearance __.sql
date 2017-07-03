9f45537693b3bdd5c203663808826dabe733d2ab
David J. Thomas <dave.a.base@gmail.com>
2017-07-02 22:48:16 -0400

    Inscription Reference data added

--- sql/views/__ Institutional Officer Other Honor Appearance __.sql
+++ sql/views/__ Institutional Officer Other Honor Appearance __.sql
@@ -10,6 +10,9 @@ SELECT DISTINCT institution."Institution" AS "Institution",
                 honor."Type" AS "Other Honor Type",
                 honor."Category" AS "Other Honor Origin",
                 officer_honor."Appearances" AS "Other Honor Appearances",
+                other_institution."Institution" AS "Other Institution",
+                other_institution."Category" AS "Other Institution Origin",
+                other_institution."Type" AS "Other Institution Type",
                 officer."Category" AS "Officer Origin",
                 CASE
                     WHEN officer."Category" = 'Athenian'
@@ -35,22 +38,25 @@ SELECT DISTINCT institution."Institution" AS "Institution",
                 institution."ID" AS "Institution ID",
                 office."ID" AS "Office ID",
                 honor."ID" AS "Honor ID",
+                other_institution."ID" AS "Other Institution ID",
                 officer."ID" AS "Officer ID",
                 inscription."ID" AS "Inscription ID",
                 officer."Uncertain Person" AS "Officer Uncertainty"
-FROM public."Institution Honor" AS institution_honor
-INNER JOIN public."Person Honor Display" AS officer_office ON institution_honor."Honor ID" = officer_office."Honor ID"
+FROM public."Institution Honor" AS institution_office
+INNER JOIN public."Person Honor Display" AS officer_office ON institution_office."Honor ID" = officer_office."Honor ID"
 INNER JOIN public."Person Honor Display" AS officer_honor ON officer_office."Person ID" = officer_honor."Person ID"
 AND officer_office."Honor ID" != officer_honor."Honor ID"
 INNER JOIN public."Person in Inscription" AS officer_appearance ON officer_honor."Inscription ID" = officer_appearance."Inscription ID"
 AND officer_honor."Person ID" = officer_appearance."Person ID"
-INNER JOIN public."Institution" AS institution ON institution_honor."Institution ID" = institution."ID"
-INNER JOIN public."Honor" AS office ON institution_honor."Honor ID" = office."ID"
+INNER JOIN public."Institution" AS institution ON institution_office."Institution ID" = institution."ID"
+INNER JOIN public."Honor" AS office ON institution_office."Honor ID" = office."ID"
 AND officer_office."Honor ID" = office."ID"
 INNER JOIN public."Honor" AS honor ON officer_honor."Honor ID" = honor."ID"
 INNER JOIN public."Person" AS officer ON officer_office."Person ID" = officer."ID"
 AND officer_honor."Person ID" = officer."ID"
 AND officer_appearance."Person ID" = officer."ID"
+INNER JOIN public."Institution Honor" AS institution_honor ON honor."ID" = institution_honor."Honor ID"
+INNER JOIN public."Institution" AS other_institution ON institution_honor."Institution ID" = other_institution."ID"
 INNER JOIN public."Inscription with Text"() AS inscription ON officer_honor."Inscription ID" = inscription."ID"
 ORDER BY institution."ID",
          office."ID",
--- sql/views/__ Institutional Officer Other Honor __.sql
+++ sql/views/__ Institutional Officer Other Honor __.sql
@@ -9,6 +9,9 @@ SELECT DISTINCT institution."Institution" AS "Institution",
                 honor."Type" AS "Other Honor Type",
                 honor."Category" AS "Other Honor Origin",
                 SUM(officer_honor."Appearances") AS "Other Honor Appearances",
+                other_institution."Institution" AS "Other Institution",
+                other_institution."Category" AS "Other Institution Origin",
+                other_institution."Type" AS "Other Institution Type",
                 officer."Category" AS "Officer Origin",
                 CASE
                     WHEN officer."Category" = 'Athenian'
@@ -27,23 +30,27 @@ SELECT DISTINCT institution."Institution" AS "Institution",
                 institution."ID" AS "Institution ID",
                 office."ID" AS "Office ID",
                 honor."ID" AS "Honor ID",
+                other_institution."ID" AS "Other Institution ID",
                 officer."ID" AS "Officer ID",
                 officer."Uncertain Person" AS "Officer Uncertainty"
-FROM public."Institution Honor" AS institution_honor
-INNER JOIN public."Person Honor Display" AS officer_office ON institution_honor."Honor ID" = officer_office."Honor ID"
+FROM public."Institution Honor" AS institution_office
+INNER JOIN public."Person Honor Display" AS officer_office ON institution_office."Honor ID" = officer_office."Honor ID"
 INNER JOIN public."Person Honor Display" AS officer_honor ON officer_office."Person ID" = officer_honor."Person ID"
 AND officer_office."Honor ID" != officer_honor."Honor ID"
 INNER JOIN public."Person in Inscription" AS officer_appearance ON officer_honor."Inscription ID" = officer_appearance."Inscription ID"
 AND officer_honor."Person ID" = officer_appearance."Person ID"
-INNER JOIN public."Institution" AS institution ON institution_honor."Institution ID" = institution."ID"
-INNER JOIN public."Honor" AS office ON institution_honor."Honor ID" = office."ID"
+INNER JOIN public."Institution" AS institution ON institution_office."Institution ID" = institution."ID"
+INNER JOIN public."Honor" AS office ON institution_office."Honor ID" = office."ID"
 AND officer_office."Honor ID" = office."ID"
 INNER JOIN public."Honor" AS honor ON officer_honor."Honor ID" = honor."ID"
 INNER JOIN public."Person" AS officer ON officer_office."Person ID" = officer."ID"
 AND officer_honor."Person ID" = officer."ID"
 AND officer_appearance."Person ID" = officer."ID"
-GROUP BY honor."ID", office."ID", institution."ID", officer."ID"
+INNER JOIN public."Institution Honor" as institution_honor ON honor."ID" = institution_honor."Honor ID"
+INNER JOIN public."Institution" as other_institution ON institution_honor."Institution ID" = other_institution."ID"
+GROUP BY honor."ID", office."ID", institution."ID", officer."ID", other_institution."ID"
 ORDER BY institution."ID",
          office."ID",
          honor."ID",
-         officer."ID"
+         officer."ID",
+         other_institution."ID"
