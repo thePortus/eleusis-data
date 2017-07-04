188e5967a8570f072b104595ddc9fdd8fa4e14df
David J. Thomas <dave.a.base@gmail.com>
2017-07-04 12:13:55 -0400

    Sponsor view updated, Sponsorship of Honorand added

--- sql/views/__ Inscription Sponsorship of Honorand __.sql
+++ sql/views/__ Inscription Sponsorship of Honorand __.sql
@@ -1,6 +1,7 @@
-CREATE OR REPLACE VIEW public."__ Inscription Sponsor __" AS
+CREATE OR REPLACE VIEW public."__ Inscription Sponsorship of Honorand __" AS
 SELECT DISTINCT inscription."IE" AS "IE",
                 sponsor."Person" AS "Sponsor",
+                honorand."Person" AS "Honorand",
                 inscription."Inscription" AS "Inscription",
                 inscription."Object Type" AS "Object Type",
                 inscription."Inscription Type" AS "Inscription Type",
@@ -25,16 +26,34 @@ SELECT DISTINCT inscription."IE" AS "IE",
                 sponsor."Family" AS "Sponsor Family",
                 sponsor."Extended" AS "Sponsor Extended Family",
                 sponsor."Deme" AS "Sponsor Deme",
+                honorand."Category" AS "Honorand Origin",
+                CASE
+                    WHEN honorand."Category" = 'Athenian'
+                         AND honorand."Roman Citizen" = FALSE THEN 'Athenian without Roman Citizenship'
+                    WHEN honorand."Category" = 'Athenian'
+                         AND honorand."Roman Citizen" = TRUE THEN 'Athenian with Roman Citizenship'
+                    ELSE 'Non-Athenian'
+                END AS "Honorand Status",
+                NULL AS "Honorand Type",
+                honorand."Gender" AS "Honorand Gender",
+                honorand."Family" AS "Honorand Family",
+                honorand."Extended" AS "Honorand Extended Family",
+                honorand."Deme" AS "Honorand Deme",
                 inscription."ID" AS "Inscription ID",
                 sponsor."ID" AS "Sponsor ID",
-                sponsor."Uncertain Person" AS "Sponsor Uncertainty"
+                honorand."ID" AS "Honorand ID",
+                sponsor."Uncertain Person" AS "Sponsor Uncertainty",
+                honorand."Uncertain Person" AS "Honorand Uncertainty"
 FROM public."Person in Inscription" AS sponsor_inscription
+INNER JOIN public."Person in Inscription" AS honorand_inscription ON sponsor_inscription."Inscription ID" = honorand_inscription."Inscription ID"
 INNER JOIN public."Inscription Full"() AS inscription ON sponsor_inscription."Inscription ID" = inscription."ID"
 INNER JOIN public."Person" AS sponsor ON sponsor_inscription."Person ID" = sponsor."ID"
-WHERE sponsor_inscription."Role" = 'Sponsor'
+INNER JOIN public."Person" AS honorand ON honorand_inscription."Person ID" = honorand."ID"
+WHERE sponsor_inscription."Role" = 'Sponsor' AND honorand_inscription."Role" = 'Honorand' AND sponsor."ID" != honorand."ID"
 UNION ALL
 SELECT DISTINCT inscription."IE" AS "IE",
                 sponsor."Institution" AS "Sponsor",
+                honorand."Person" AS "Honorand",
                 inscription."Inscription" AS "Inscription",
                 inscription."Object Type" AS "Object Type",
                 inscription."Inscription Type" AS "Inscription Type",
@@ -53,11 +72,29 @@ SELECT DISTINCT inscription."IE" AS "IE",
                 NULL AS "Sponsor Family",
                 NULL AS "Sponsor Extended Family",
                 NULL AS "Sponsor Deme",
+                honorand."Category" AS "Honorand Origin",
+                CASE
+                    WHEN honorand."Category" = 'Athenian'
+                         AND honorand."Roman Citizen" = FALSE THEN 'Athenian without Roman Citizenship'
+                    WHEN honorand."Category" = 'Athenian'
+                         AND honorand."Roman Citizen" = TRUE THEN 'Athenian with Roman Citizenship'
+                    ELSE 'Non-Athenian'
+                END AS "Honorand Status",
+                NULL AS "Honorand Type",
+                honorand."Gender" AS "Honorand Gender",
+                honorand."Family" AS "Honorand Family",
+                honorand."Extended" AS "Honorand Extended Family",
+                honorand."Deme" AS "Honorand Deme",
                 inscription."ID" AS "Inscription ID",
                 sponsor."ID" AS "Sponsor ID",
-                sponsor_inscription."Uncertain" AS "Sponsor Uncertainty"
+                honorand."ID" AS "Honorand ID",
+                sponsor_inscription."Uncertain" AS "Sponsor Uncertainty",
+                honorand."Uncertain Person" AS "Honorand Uncertainty"
 FROM public."Institution Sponsorship" AS sponsor_inscription
+INNER JOIN public."Person in Inscription" as honorand_inscription ON sponsor_inscription."Inscription ID" = honorand_inscription."Inscription ID"
 INNER JOIN public."Inscription Full"() AS inscription ON sponsor_inscription."Inscription ID" = inscription."ID"
 INNER JOIN public."Institution" AS sponsor ON sponsor_inscription."Institution ID" = sponsor."ID"
+INNER JOIN public."Person" AS honorand ON honorand_inscription."Person ID" = honorand."ID"
+WHERE honorand_inscription."Role" = 'Honorand'
 ORDER BY "Inscription ID",
          "Sponsor ID"
